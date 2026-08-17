e907921dc01961ead1b7f04958d0eef7675d2ff3
princeonpurpose <princeonpurpose@gmail.com>
2026-08-17 14:58:46 -0700

    Update config.js

--- waitlist/admin/config.js
+++ waitlist/admin/config.js
@@ -3,5 +3,5 @@
 // Supabase RLS protects the data. Never put a service-role/secret key here.
 window.PERSAT_SUPABASE_CONFIG = {
   url: "https://mipalifltoqppvamtemg.supabase.co",
-  anonKey: "sb_publishable_826Ab-oV-EK9CwU398Ttsw_-00rctA9"
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pcGFsaWZsdG9xcHB2YW10ZW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1NTg0MzksImV4cCI6MjA4MzEzNDQzOX0.89-hcshYp0qVW2i2fIOF9M9yXS11JAWGMXwThvLbJE4"
 };
